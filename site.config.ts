2fa31fa552fee4deb6915c3b9e07ab0c15f6d1bb
oeyoews <jyao4783@gmail.com>
2022-12-13 15:53:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '0f2a3f9d-839e-464b-9653-662330059058',
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
