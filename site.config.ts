198e23f00900cc59c6600873485ea71e68db6ca7
oeyoews <jyao4783@gmail.com>
2022-12-17 02:04:56 +0800

    Automatic commit

--- site.config.ts
+++ site.config.ts
@@ -33,8 +33,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: 'https://oeyoewl.top/around.png', // cant with dash ???
+  defaultPageCover: 'https://oeyoewl.top/cover.png',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
